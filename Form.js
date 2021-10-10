d9f4189f87630990f6156ffe79e9e8c37e83e637
Gurparas-Singh <78079839+Gurparas-Singh@users.noreply.github.com>
2021-10-10 05:34:50 +0530

    Update Form.js

--- Form.js
+++ Form.js
@@ -7,13 +7,13 @@ class Form{
         
         var button1 = createButton("Feed Pet");
         var button2 = createButton("Add Food");
-        var input = createInput("Pet Name");
+
 
         text("FeedTime: "+lastFed,180,125)
        
         button1.position(400,500);
         button2.position(725,500);
-        input.position(500,180);
+
 
         if(foodS>0){
         button1.mousePressed(function(){
